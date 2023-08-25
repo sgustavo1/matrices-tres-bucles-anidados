7f0953cef36ec1f0ba04bad6b39d8d88092fc4e6
sgustavo1 <135420325+sgustavo1@users.noreply.github.com>
2023-08-24 23:40:37 -0500

    Update bucless.cpp

--- bucless.cpp
+++ bucless.cpp
@@ -1,44 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <random>
 
 using namespace std;
 using namespace std::chrono;
 
 const int BLOCK_SIZE = 32;
-const int NUM_REPETITIONS = 5;
 
-void Multiply(int n, const vector<vector<double>>& a, const vector<vector<double>>& b, vector<vector<double>>& c) {
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            c[i][j] = 0.0;  // Resetear la matriz resultado
-            for (int k = 0; k < n; ++k) {
-                c[i][j] += a[i][k] * b[k][j];
+// Función para realizar la multiplicación de matrices con bloques
+void MultiplyByBlocks(int n, vector<vector<double>>& a, vector<vector<double>>& b, vector<vector<double>>& c) {
+    for (int bi = 0; bi < n; bi += BLOCK_SIZE) {
+        for (int bj = 0; bj < n; bj += BLOCK_SIZE) {
+            for (int bk = 0; bk < n; bk += BLOCK_SIZE) {
+                for (int i = 0; i < BLOCK_SIZE; i++) {
+                    for (int j = 0; j < BLOCK_SIZE; j++) {
+                        for (int k = 0; k < BLOCK_SIZE; k++) {
+                            c[bi + i][bj + j] += a[bi + i][bk + k] * b[bk + k][bj + j];
+                        }
+                    }
+                }
             }
         }
     }
 }
 
 int main() {
-    // ... (parte inicial del código sin cambios)
+    int n;
+    cout << "Introduce la dimensión n de la matriz: ";
+    cin >> n;
 
-    auto startTotal = high_resolution_clock::now();  // Marcar el tiempo de inicio total
+    vector<vector<double>> A(n, vector<double>(n, 1.0)); // Llenamos A con 1.0
+    vector<vector<double>> B(n, vector<double>(n, 2.0)); // Llenamos B con 2.0
+    vector<vector<double>> C(n, vector<double>(n, 0.0)); // Inicializamos C con 0.0
+
+    double elapsed = 0.0;
+    const int NUM_REPETITIONS = 5;
+
+    // "Calentamiento" inicial para preparar la caché
+    MultiplyByBlocks(n, A, B, C);
+
+    cout << "Multiplicando matrices " << NUM_REPETITIONS << " veces..." << endl;
 
     for (int i = 0; i < NUM_REPETITIONS; ++i) {
-        auto start = high_resolution_clock::now();  // Marca el tiempo de inicio
-        Multiply(n, A, B, C);  // Llama a la función de multiplicación
-        auto stop = high_resolution_clock::now();  // Marca el tiempo de finalización
-        auto duration = duration_cast<microseconds>(stop - start);  // Calcula la duración en microsegundos
-        cout << "Tiempo de la repetición " << (i + 1) << " = " << duration.count() << " microsegundos" << endl;
-        elapsed += duration.count();
+        auto start = high_resolution_clock::now();
+        MultiplyByBlocks(n, A, B, C);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        elapsed += duration.count() / 1e6;
+        if (i == 0) {
+            cout << "Primera repetición tomó " << elapsed << " segundos" << endl;
+        }
     }
 
-    auto stopTotal = high_resolution_clock::now();  // Marcar el tiempo de finalización total
-    auto durationTotal = duration_cast<microseconds>(stopTotal - startTotal);  // Calcular la duración total en microsegundos
-    double averageTime = static_cast<double>(durationTotal.count()) / NUM_REPETITIONS;
+    double averageTime = elapsed / NUM_REPETITIONS;
 
-    cout << "Tiempo total = " << elapsed << " microsegundos" << endl;
-    cout << "Tiempo promedio por operación = " << averageTime << " microsegundos" << endl;
+    cout << "Tiempo promedio por operación (por bloques) = " << averageTime << " segundos" << endl;
 
     return 0;
 }
