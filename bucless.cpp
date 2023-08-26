dfd831e058dd3cde51b952fc4159cbe0a7b1815d
sgustavo1 <135420325+sgustavo1@users.noreply.github.com>
2023-08-26 11:45:52 -0500

    Update bucless.cpp

--- bucless.cpp
+++ bucless.cpp
@@ -10,34 +10,34 @@ using namespace std::chrono;
 // Función para multiplicar dos matrices
 vector<vector<int>> multiplyMatrices(const vector<vector<int>>& matrixA, const vector<vector<int>>& matrixB) {
     int size = matrixA.size();
-    vector<vector<int>> result(size, vector<int>(size, 0));
+    vector<vector<int>> result(size, vector<int>(size, 0)); // Inicializar la matriz de resultados con ceros
 
     // Realizar la multiplicación de matrices utilizando tres bucles anidados
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
             for (int k = 0; k < size; ++k) {
-                result[i][j] += matrixA[i][k] * matrixB[k][j];
+                result[i][j] += matrixA[i][k] * matrixB[k][j]; // Realizar la suma acumulativa de los productos
             }
         }
     }
 
-    return result;
+    return result; // Devolver la matriz resultante
 }
 
 int main() {
     int size;
 
     // Solicitar al usuario ingresar el tamaño de la matriz cuadrada
-    cout << "Ingrese el tamaño de la matriz cuadrada: ";
+    cout << "Ingrese el tamaño de la matriz cuadrada (por ejemplo, 2 para 2x2, 3 para 3x3): ";
     cin >> size;
 
     if (size <= 0) {
-        cerr << "El tamaño de la matriz debe ser un número positivo." << endl;
-        return 1;
+        cerr << "El tamaño de la matriz debe ser un número positivo." << endl; // Mostrar un error si el tamaño es no positivo
+        return 1; // Salir del programa con un código de error
     }
 
     int numRepetitions = 5; // Número de repeticiones
-    double totalElapsedTime = 0.0; // Tiempo total de ejecución
+    double totalElapsedTime = 0.0; // Inicializar el tiempo total de ejecución
 
     // Realizar la multiplicación de matrices y medir el tiempo en cada repetición
     for (int repetition = 1; repetition <= numRepetitions; ++repetition) {
@@ -48,12 +48,12 @@ int main() {
         // Llenar las matrices con valores aleatorios usando un generador más eficiente
         random_device rd;
         mt19937 gen(rd());
-        uniform_int_distribution<int> dis(1, 100); // Valores entre 1 y 100
+        uniform_int_distribution<int> dis(1, 100); // Generar valores aleatorios entre 1 y 100
 
         for (int i = 0; i < size; ++i) {
             for (int j = 0; j < size; ++j) {
-                matrixA[i][j] = dis(gen);
-                matrixB[i][j] = dis(gen);
+                matrixA[i][j] = dis(gen); // Llenar la matriz A con valores aleatorios
+                matrixB[i][j] = dis(gen); // Llenar la matriz B con valores aleatorios
             }
         }
 
@@ -75,5 +75,5 @@ int main() {
     // Mostrar el tiempo total de ejecución de todas las repeticiones
     cout << "Tiempo total de ejecución de las " << numRepetitions << " repeticiones: " << totalElapsedTime << " segundos" << endl;
 
-    return 0;
+    return 0; // Salir del programa con un código de éxito
 }
